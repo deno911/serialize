79077987e6e3de8d0dcdc357c7b5b79b2a3bf1cb
Nicholas Berlette <nick@berlette.com>
2022-12-25 07:36:17 +0000

    feat: add deps.ts

---
+++ deps.ts
@@ -0,0 +1,22 @@
+
+export {
+  type Assert,
+  assert,
+  is,
+  type TypeName,
+} from "https://x.nest.land/dis@0.3.0-rc.3/mod.ts";
+
+export { $ } from "https://deno.land/x/dax@0.17.0/mod.ts";
+
+export { colors } from "https://deno.land/x/cliffy@v0.25.6/ansi/mod.ts";
+
+export * from "https://deno.land/std@0.170.0/fs/mod.ts";
+
+export * as semver from "https://deno.land/std@0.170.0/semver/mod.ts";
+
+export * as JSONC from "https://deno.land/std@0.170.0/encoding/jsonc.ts";
+
+export * as YAML from "https://deno.land/std@0.170.0/encoding/yaml.ts";
+
+export * as TOML from "https://deno.land/std@0.170.0/encoding/toml.ts";
+
